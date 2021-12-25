3d77996325428d81332a1d6fa6b9726f05ee6d7d
gituser01 <gituser01.com>
2021-12-25 16:32:29 +0800

    20211225接口测试

---
+++ src/main/java/com/domain.java
@@ -0,0 +1,18 @@
+package com;
+
+import org.testng.annotations.Test;
+
+/**
+ * Created by pei hao on 2021/12/25.
+ */
+public class domain {
+
+    @Test(priority = 0)
+    public void test001(){
+        System.out.println("1111111111");
+    }
+    @Test(priority = 1)
+    public void test002(){
+        System.out.println("22222222");
+    }
+}
